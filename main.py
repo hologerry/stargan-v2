9ac3e36197ac4980935694d19e3ae01685d983d0
Gerry Gao <hologerry@gmail.com>
2020-06-11 16:32:58 +0800

    update path

--- main.py
+++ main.py
@@ -169,8 +169,8 @@ if __name__ == '__main__':
                         help='output directory when aligning faces')
 
     # face alignment
-    parser.add_argument('--wing_path', type=str, default='expriments/starganv2/checkpoints/wing.ckpt')
-    parser.add_argument('--lm_path', type=str, default='expriments/starganv2/checkpoints/celeba_lm_mean.npz')
+    parser.add_argument('--wing_path', type=str, default='stargan-v2/checkpoints/wing.ckpt')
+    parser.add_argument('--lm_path', type=str, default='stargan-v2/checkpoints/celeba_lm_mean.npz')
 
     # step size
     parser.add_argument('--print_every', type=int, default=10)
